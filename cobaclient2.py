51bb7c5261f9df2d1e8960adfa01aba695fd70a5
AndiPutra31 <andiputra3107@gmail.com>
2015-10-11 09:24:16 +0700

    Update cobaclient2.py

--- cobaclient2.py
+++ cobaclient2.py
@@ -2,13 +2,15 @@ import sys
 import socket
 import select
  
-def chat_client():
-    if(len(sys.argv) < 3) :
-        print 'Usage : python chat_client.py hostname port'
-        sys.exit()
+#def chat_client():
+ #   if(len(sys.argv) < 3) :
+  #      print 'Usage : python chat_client.py hostname port'
+   #     sys.exit()
 
-    host = sys.argv[1]
-    port = int(sys.argv[2])
+    host = 'localhost'
+    port = 9009
+    #host = sys.argv[1]
+    #port = int(sys.argv[2])
      
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(2)
@@ -20,7 +22,7 @@ def chat_client():
         print 'Unable to connect'
         sys.exit()
      
-    print 'Connected to remote host. You can start sending messages'
+    print 'Connected...'
     sys.stdout.write('[Me] '); sys.stdout.flush()
      
     while 1:
